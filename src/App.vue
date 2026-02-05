e3aea118e18810f41fe0d2ce8bc1b4782a96f168
nxstray <afwanapriansyah@gmail.com>
2026-02-06 02:10:12 +0700

    chore: add speed insight package for web performance in vercel

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import { SpeedInsights } from "@vercel/speed-insights/vue"
 </script>
 
 <template>
   <RouterView />
+  <SpeedInsights />
 </template>
 
 <style>
